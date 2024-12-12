178335f4ca3cd4056c4bbc5a90b27731e058d37c
Isaias Mendes <isaiaszmendes@gmail.com>
2024-12-12 18:12:15 -0300

    add configuration and logging implementation with GORM integration

--- projects/api/main.go
+++ projects/api/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/isaiaszmendes/gopportunities/router"
+import (
+	"github.com/isaiaszmendes/gopportunities/config"
+	"github.com/isaiaszmendes/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize the configuration
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing the configuration: %v", err)
+
+		panic(err) // panic é uma função que interrompe a execução do programa
+		// Nesse caso faz sentido pois se o banco de dados não estiver disponível, não faz sentido executar o programa
+	}
+
 	// Initialize the router
 	router.Initialize()
 }
